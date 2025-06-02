2371c7d126de803653781ffe19cef8dcf82dd20c
kotappa19 <kgandudi@gmail.com>
2025-06-02 20:19:13 +0530

    Merge dev branch

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -92,6 +92,7 @@ const config: Config = {
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
+        marquee: 'marquee 15s linear infinite',
         slowspin: "spin 10s linear infinite", 
       },
     },
